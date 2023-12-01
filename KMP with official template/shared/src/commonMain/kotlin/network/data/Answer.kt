940a40ab564a1b8f703c4261d5617f01ed6b8fdc
A187839 <ibrahim.gharbi@worldline.com>
2023-12-01 21:18:00 +0100

    merged code

--- KMP with official template/shared/src/commonMain/kotlin/network/data/Answer.kt
+++
@@ -1,6 +0,0 @@
-package network.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Answer(val id: Int, val label: String )
--- KMP with official template/shared/src/iosMain/kotlin/main.ios.kt
+++
@@ -1,5 +0,0 @@
-import moe.tlaster.precompose.PreComposeApplication
-
-actual fun getPlatformName(): String = "iOS"
-
-fun MainViewController() = PreComposeApplication() { App() }
