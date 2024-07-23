2e943f2a5d8b8bf43310bb84cf96dc09258332c1
Ibrahim Gharbi <brah.gharbi@gmail.com>
2024-07-23 09:56:06 +0200

    deleted files

--- desktopApp/src/jvmMain/kotlin/main.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import moe.tlaster.precompose.PreComposeWindow
-
-fun main() = application {
-    PreComposeWindow(onCloseRequest = ::exitApplication, title = "QuizApp") {
-        MainView()
-    }
-}
