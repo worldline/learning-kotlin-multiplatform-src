cd5d77cd968945697414e869751159a16a6e2da4
yostane <1958676+yostane@users.noreply.github.com>
2024-08-27 16:11:42 +0200

    Fix build error

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,4 +1,3 @@
-
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -8,7 +7,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import kotlinx.datetime.Clock
 import screens.questionScreen
 import screens.scoreScreen
 import screens.welcomeScreen
@@ -16,55 +14,53 @@ import screens.welcomeScreen
 @Composable
 fun App(
     viewModel: QuizViewModel = viewModel { QuizViewModel() },
-    navController: NavHostController = rememberNavController()) {
-    
-        MaterialTheme {
-            NavHost(
-                navController = navController,
-                startDestination = "/welcome",
-            ) {
-                composable(route = "/welcome") {
-                    welcomeScreen(
-                        onStartButtonPushed = {
-                            navController.navigate(route = "/quiz")
+    navController: NavHostController = rememberNavController()
+) {
+
+    MaterialTheme {
+        NavHost(
+            navController = navController,
+            startDestination = "/welcome",
+        ) {
+            composable(route = "/welcome") {
+                welcomeScreen(
+                    onStartButtonPushed = {
+                        navController.navigate(route = "/quiz")
+                    }
+                )
+            }
+            composable(route = "/quiz") {
+
+                val questions by viewModel.questionState.collectAsState()
+
+                if (questions.isNotEmpty()) {
+                    questionScreen(
+                        questions = questions,
+                        onFinishButtonPushed = { score: Int, questionSize: Int ->
+
+                            /* FOR SPEAKER TALK DEMO ON WEB APP */ if (getPlatform().toString() == "WASM") viewModel.postStats(
+                            score,
+                            "user-${(0..1000).random()}"
+                        )
+                            navController.navigate(route = "/score/$score/$questionSize")
+                        },
+                        /* FOR SPEAKER TALK DEMO ON WEB APP */
+                        onSaveStatQuestion = { id: Long, question: String, anwserId: Long, correctAnwserId: Long, answer: String ->
+                            viewModel.addStats(id, question, anwserId, correctAnwserId, answer)
                         }
                     )
                 }
-                composable(route = "/quiz") {
-                    
-                    val questions by viewModel.questionState.collectAsState()
-
-                    if (questions.isNotEmpty()) {
-                        questionScreen(
-                            questions = questions,
-                            onFinishButtonPushed = { score:Int,questionSize:Int ->
-
-                                /* FOR SPEAKER TALK DEMO ON WEB APP */ if(getPlatform().toString() == "WASM") viewModel.postStats(score,"user-${(0..1000).random()}")
-                                navController.navigate(route="/score/$score/$questionSize")
-                            },
-                            /* FOR SPEAKER TALK DEMO ON WEB APP */
-                            onSaveStatQuestion = {
-                                id: Long, question: String,  anwserId: Long,  correctAnwserId: Long,  answer: String ->
-                                viewModel.addStats(id,question,anwserId, correctAnwserId, answer)
-                            }
-                        )
-                    }
-                }
-                composable( route = "/score/{score}/{total}") {
-                        scoreScreen(
-                            score= it.arguments?.getString("score").toString(),
-                            total= it.arguments?.getString("total").toString(),
-                            onResetButtonPushed = {
-                               navController.navigate(route = "/quiz")
-                            }
-                        )
-                }
-                    
             }
+            composable(route = "/score/{score}/{total}") {
+                scoreScreen(
+                    score = it.arguments?.getString("score").toString(),
+                    total = it.arguments?.getString("total").toString(),
+                    onResetButtonPushed = {
+                        navController.navigate(route = "/quiz")
+                    }
+                )
+            }
+
         }
-}
-
-fun statsEnabled():Boolean{
-
-
+    }
 }
